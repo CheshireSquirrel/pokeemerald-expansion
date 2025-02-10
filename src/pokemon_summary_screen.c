ae5f8a7b4c58a07d998503774c02dc27fd1f3919
CheshireSquirrel <cheshquirrel@gmail.com>
2025-02-09 23:59:45 -0800

    Minor tweaks
    Fixed nature colors and activated the in-battle type tabs.

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3567,10 +3567,12 @@ static void PrintRibbonCount(void)
 
 static void BufferStat(u8 *dst, u8 statIndex, u32 stat, u32 strId, u32 n)
 {
-    static const u8 sTextNatureDown[] = _("{COLOR}{08}");
-    static const u8 sTextNatureUp[] = _("{COLOR}{05}");
+    static const u8 sTextNatureDown[] = _("{COLOR}{09}");
+    static const u8 sTextNatureUp[] = _("{COLOR}{07}");
     static const u8 sTextNatureNeutral[] = _("{COLOR}{01}");
     u8 *txtPtr;
+
+
 
     if (statIndex == 0 || !SUMMARY_SCREEN_NATURE_COLORS || gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown)
         txtPtr = StringCopy(dst, sTextNatureNeutral);
